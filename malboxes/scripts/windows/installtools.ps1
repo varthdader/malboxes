88fa80592c08d9de97631cb17bd38c37c8b7ef1c
Varth Dader <varthdader@users.noreply.github.com>
2024-05-06 17:40:14 -0600

    Update installtools.ps1
    Updated AutoHotKey Installation

--- malboxes/scripts/windows/installtools.ps1
+++ malboxes/scripts/windows/installtools.ps1
@@ -2,5 +2,5 @@
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # For some reason, AutoHotkey.portable wasn't working with WinPcap, so use the full installation
-cinst autohotkey -y
+choco install autohotkey -y
 $env:Path = "$($env:Path)C:\Program Files\AutoHotkey;" 
